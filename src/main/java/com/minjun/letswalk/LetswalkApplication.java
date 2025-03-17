6b886ae90671bf304bc2d6f1c913f1d85686af71
minjund <mam060922@naver.com>
2025-03-17 23:34:36 +0900

    초기 세팅

---
+++ src/main/java/com/minjun/letswalk/LetswalkApplication.java
@@ -0,0 +1,13 @@
+package com.minjun.letswalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetswalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetswalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minjun/letswalk/LetswalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjun.letswalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetswalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
