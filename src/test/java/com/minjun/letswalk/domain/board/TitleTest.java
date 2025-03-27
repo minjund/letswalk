262975fbed7a07ed8887064f0985b5589288447b
minjund <mam060922@naver.com>
2025-03-27 23:54:28 +0900

    feat : displayName 추가

--- src/test/java/com/minjun/letswalk/domain/board/TitleTest.java
+++ src/test/java/com/minjun/letswalk/domain/board/TitleTest.java
@@ -1,5 +1,6 @@
 package com.minjun.letswalk.domain.board;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TitleTest {
 
+    @DisplayName("제목 객체를 생성 한 값을 확인 한다.")
     @Test
     void getName() {
         Title title = new Title("아아");
@@ -14,10 +16,11 @@ class TitleTest {
         assertThat(title.valueOf()).isEqualTo("아아");
     }
 
+    @DisplayName("제목 길이가 20자를 넘어 갈 수 없다.")
     @Test
     void lengthCheck() {
         assertThatThrownBy(() -> {
-            new Title("12345678901234567890");
+            new Title("1234567890123456789011");
         })
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("20자 이상 제목을 입력할 수 없습니다.");
